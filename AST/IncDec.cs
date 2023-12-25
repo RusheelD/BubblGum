0b1403947cadc0114f201adc4b6452645a304740
Rusheel Dasari <rusheel@gmail.com>
2023-12-25 02:09:01 -0800

    added new nodes

---
+++ AST/IncDec.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AST
+{
+    public class IncDec : Exp
+    {
+        public Exp E1;
+        public bool IncOrDec;
+        public bool PreOrPost;
+
+        public IncDec(Exp e1, bool incOrDec, bool preOrPost, int lineNumber, int startCol)
+        {
+            E1 = e1;
+            IncOrDec = incOrDec;
+            PreOrPost = preOrPost;
+            LineNumber = lineNumber;
+            StartCol = startCol;
+        }
+    }
+}
---
+++ AST/Tuple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AST
+{
+    public class Tuple : Exp
+    {
+        public List<Exp> Exps;
+
+        public Tuple(List<Exp> exps, int lineNumber, int startCol)
+        {
+            Exps = exps;
+            LineNumber = lineNumber;
+            StartCol = startCol;
+        }
+    }
+}
