719e91be6dc5efa0684de0380e949e9ced646b68
Raghav Narula <raghavnarula.77@gmail.com>
2024-01-06 18:10:04 -0800

    added genAll command

--- commands.bat
+++ commands.bat
@@ -9,6 +9,10 @@ IF "%1"=="gen" (
 ) ELSE IF "%1"=="genHeader" (
     REM compiles and registers ANTLR grammar storing imports + namespaces (used to link files efficiently)
     antlr4 -o ./Preprocessing/Parser ./Preprocessing/Parser/BubblGumHeader.g4 -Dlanguage=CSharp
+) ELSE IF "%1"=="genAll" (
+    REM runs gen and genHeader
+    antlr4 -o ./Lexer_Parser/Parser ./Lexer_Parser/Parser/BubblGum.g4 -Dlanguage=CSharp
+    antlr4 -o ./Preprocessing/Parser ./Preprocessing/Parser/BubblGumHeader.g4 -Dlanguage=CSharp
 ) ELSE IF "%1"=="run" (
     REM runs compiler
     dotnet run --project ./Main/Main.csproj "run" "%2" "%3"
